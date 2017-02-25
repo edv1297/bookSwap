17ac4c2af0ac13147a386d3f57359785b407b59a
KAHerbst <kah3@williams.edu>
2017-02-25 17:27:11 -0500

    Update Course.java

--- Course.java
+++ Course.java
@@ -1,52 +1,59 @@
 public class Course{
-  //course objects with a constructed array of book objects(filled from a scraper)
-  public Course(String aTitle, String aDepartment, int courseNum, array Book[], int aSection){
-    String title = aTitle;
-    String department = aDepartment;
-    int num = courseNum;
-    Array books = Book[];
-    int section = aSection;
+  String title;
+  String listing;
+  int num;
+  BookCollection bookList;
+  int section;
+//course objects with a constructed array of book objects(filled from a scraper)
+  public Course(String aTitle, String courseTitle, BookCollection books, int aSection){
+  title = aTitle;
+  listing = courseTitle;
+  bookList = books;
+  section = aSection;
+  }
+  public String getTitle(){
+    return title;
     }
-    
-    public String getTitle(){
-      return this.title;
+
+  public int getListing(){
+    return num;
+  }
+
+  public String getDepartment(){
+    return department;
+  }
+
+  public int getSection(){
+    return section;
+  }
+
+  public String toString(){
+    String courseDescription = "Title: " + getTitle + " " + getListing + " Books requiered" +  ;
+
+    for(int bookNum = 0; bookNum < bookList.length; bookNum++){
+      books+= (bookList[bookNum] + "\n");
       }
-    
-    public int getNum(){
-      return this.num;
-    }
-    
-    public getDepartment(){
-      return this.department;
-    }
-    
-    public int getSection(){
-      return this.section;
-    }
-    
-    public String getBooks(){
-      bookslist = "";
-      for(int bookNum = 0; bookNum < this.books.size(); bookNum++){
-        bookList += (" " + books.get(bookNum));
-        }
-      return booksList
-    }
-    
-    public vector allCourses(){
-      //Scraper use to store all populated courses with their populated arrays in a vector
-    }
-    
-    public String toString(){
-      String cBooks = "Your required book(s) are: ";
-      return (cBooks + this.getBooks());
-    }
-    
-    public static void main(String[] args){
-       public Course(String aTitle, String aDepartment, int courseNum, array Book[], int aSection);
-      Course("Across the Oceans and Seas", "COMP", 358, bookList1, 01);
-      Course("Linear Algebra", "MATH", 250, bookList2, 02);
-      Course("World Politics: An Introduction to International Relations", "PSCI", 202, bookList3 , 01);
-      Course("Price and Allocation Theory", "ECON" , 251, bookList4 , 02);
+    return books;
+  }
+
+  /*public vector allCourses(){
+    //Scraper use to store all populated courses with their populated arrays in a vector
+  }*/
+
+  public String toString(){
+    String cBooks = "Your required book(s) are: ";
+    return (cBooks + this.getBooks());
+  }
+}
+  public static void main(String[] args){
   
+    
+    for (int i = 1; i<5; i++){
+      System.out.println( bookList1[i].toString());
     }
+    //System.out.println(bookList2);
+    //System.out.println(bookList3);
+    //System.out.println(bookList4);
+  
+}
 }
