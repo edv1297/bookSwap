c8b997b9c0979c29308211e3787de37adea92d76
KAHerbst <kah3@williams.edu>
2017-02-25 16:55:29 -0500

    Create bookSwap.java

---
+++ bookSwap.java
@@ -0,0 +1,34 @@
+import java.util.Scanner.*;
+
+public class bookSwap{
+protected Course a;
+
+
+public static void main(String args[]){
+
+  Scanner scan = new Scanner(System.in);
+
+  System.out.println("Are you a buyer or a seller?");
+  String respone = scan.nextLine().toLowerCase();
+
+if(response.equals("buyer")){
+  System.out.println("What class are you looking for?");
+  String class = scan.nextLine();
+  System.out.println("What section are you in?")
+  int section = scan.nextLine();
+  Buyer aBuyer = new Buyer();
+}else if(response.equals("seller")){
+
+System.out.println("What class are you selling for?")
+  Seller seller = new Seller();
+}
+  System.out.println("What ")
+
+
+
+  a = new Course("Across the Oceans and Seas", "COMP 358", bookList1, 01);
+System.out.println("")
+
+
+
+}
