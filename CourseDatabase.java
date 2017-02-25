c54c1e2975a721e66720e130aaf3665d4084073c
KAHerbst <kah3@williams.edu>
2017-02-25 18:47:08 -0500

    Update CourseDatabase.java

--- CourseDatabase.java
+++ CourseDatabase.java
@@ -3,10 +3,9 @@ import structure5.*;
 public class CourseDatabase{
   protected Vector<Course> courses;
   protected Course newCourse;
-  public CourseDatabase(Course aCourse){
+  public CourseDatabase(){
     newCourse = aCourse;
     courses = new Vector<Course>();
-    populateDatabase(newCourse);
   }
   public void populateDatabase(Course course){
     courses.add(course);
