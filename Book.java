bdc32003720087c05fef108b9533b299982e8dc1
edv1297 <edv2@williams.edu>
2017-02-25 09:05:32 -0500

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,38 @@
+public class Book {
+
+    protected String title;
+    protected String author;
+    protected int ISBN;
+    protected double price;
+    
+    public Book (String aTitle, String anAuthor, int anISBN, double aPrice) {
+        title = aTitle;
+        author = anAuthor;
+        ISBN = anISBN;
+        price = aPrice;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getISBN() {
+        return ISBN;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String toString() {
+        return title + " by " + author + " (" + ISBN + ") sells for " + price;
+    }
+
+    public void setPrice (double newPrice) {
+        price = newPrice;
+    }
+}
